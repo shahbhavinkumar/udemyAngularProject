084e3f690cad189315a5ac61acdf96fb9cb3ad19
Bhavin Shah <bshah@beamsoftware.com>
2018-11-13 11:50:39 -0500

    13Nov Section 03: Course Basics

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Ingredient } from './ingredient.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,7 +8,10 @@ import { Ingredient } from './ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
 
-  public ingredients:Ingredient[] = [];
+  public ingredients:Ingredient[] = [
+    new Ingredient('Raw Pasta', 12),
+    new Ingredient('Raw Spegatti',10)
+  ];
   constructor() { }
 
   ngOnInit() {
