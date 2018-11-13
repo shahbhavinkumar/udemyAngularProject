395f79c89504efc079f9568bba661fea2cbeb0ae
Bhavin Shah <bshah@beamsoftware.com>
2018-11-13 16:40:03 -0500

    13Nov Section 06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public loadedFeature ='recipe';
+  public onNavigate(feature:string){
+
+  }
 }
