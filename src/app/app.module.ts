98285be5bffdb2846146070701da29ebb469c563
Bhavin Shah <bshah@beamsoftware.com>
2018-11-15 10:47:00 -0500

    15Nov : Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     RecipeListComponent,
     RecipeDetailComponent,
     ShoppingListComponent, 
-    ShoppingEditComponent, RecipeItemComponent
+    ShoppingEditComponent, RecipeItemComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
